d4f8f08223d68811fec6a8685d6c3ece6540a1cc
Zachary Arney <zachary.arney@gmail.com>
2024-01-09 21:58:42 -0800

    Adds histogram algorithm

--- src/colors/colorPalette.ts
+++ src/colors/colorPalette.ts
@@ -1,36 +1,63 @@
 import sharp from 'sharp';
+import Histogram from './histogram';
+
+type channelString = 'red' | 'green' | 'blue';
+
+type redValue = Uint8ClampedArray;
+type greenValue = Uint8ClampedArray;
+type blueValue = Uint8ClampedArray;
+
+export interface Pixel {
+  r: number;
+  g: number;
+  b: number;
+}
+
+export type ColorChannels = [redValue, greenValue, blueValue];
+
+export interface PaletteAlgorithm {
+  buildPalette(channels: ColorChannels): Pixel[];
+}
 
 class ColorPalette {
   imageUrl: string;
-  redChannel: Uint8ClampedArray = new Uint8ClampedArray();
-  greenChannel: Uint8ClampedArray = new Uint8ClampedArray();
-  blueChannel: Uint8ClampedArray = new Uint8ClampedArray();
+  colorChannels: ColorChannels = [
+    new Uint8ClampedArray(),
+    new Uint8ClampedArray(),
+    new Uint8ClampedArray(),
+  ];
+  algorithm: PaletteAlgorithm;
+  histogram: Pixel[] = [];
 
   constructor(imageUrl: string) {
     this.imageUrl = imageUrl;
+    this.algorithm = new Histogram();
+  }
+
+  async loadHistogram(dimensions = 3, paletteSize = 10) {
+    await this.loadChannels();
+    this.algorithm = new Histogram(paletteSize, dimensions);
+    this.histogram = this.algorithm.buildPalette(this.colorChannels);
   }
 
   async loadChannels() {
     try {
-      const red = await sharp(this.imageUrl).
-        extractChannel('red').
-        raw().
-        toBuffer();
-      const green = await sharp(this.imageUrl).
-        extractChannel('green').
-        raw().
-        toBuffer();
-      const blue = await sharp(this.imageUrl).
-        extractChannel('blue').
-        raw().
-        toBuffer();
+      const red: Uint8ClampedArray = await this.getChannel('red');
+      const green: Uint8ClampedArray = await this.getChannel('green');
+      const blue: Uint8ClampedArray = await this.getChannel('blue');
 
-      this.redChannel = new Uint8ClampedArray(red.buffer);
-      this.greenChannel = new Uint8ClampedArray(green.buffer);
-      this.blueChannel = new Uint8ClampedArray(blue.buffer);
+      this.colorChannels = [red, green, blue];
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async getChannel(channel: channelString) {
+    const channelData = await sharp(this.imageUrl)
+      .extractChannel(channel)
+      .raw()
+      .toBuffer();
+    return new Uint8ClampedArray(channelData);
   }
 }
 
